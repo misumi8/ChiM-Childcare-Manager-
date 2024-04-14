90a22fefbb154eb806907f30daab95c13c4f0236
GeorgescuRazvan <126411884+GeorgescuRazvan@users.noreply.github.com>
2024-04-14 23:19:37 +0300

    plus icon changed

--- public/profile.php
+++ public/profile.php
@@ -14,7 +14,7 @@ require_once '../includes/header.php';
     <div class="profile-child-container">
         <img src="../users/user1/user1_profile.jpg">
         <span>Child Name</span>
-        <a><img class="profile-plus" src="../plus.png"></a>
+        <a href="#"><img class="profile-plus" src="../plus.png"></a>
     </div>
 </div>
 

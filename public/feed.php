e25995990ffc3cd1240a1d0ebf2ed9b70eb3b34d
GeorgescuRazvan <126411884+GeorgescuRazvan@users.noreply.github.com>
2024-04-14 23:07:45 +0300

    started profile and made feed tweaks

--- public/feed.php
+++ public/feed.php
@@ -4,24 +4,30 @@ require_once '../includes/header.php';
 <div class="feed-background">
 
 <div class="feed-content-container">
-<div class="feed-account-name"><img src="../users/user1/user1_profile.jpg" alt="Profile picture"> UserAccountName</div>
-<div class="feed-content-media"><img src="../users/user1/child1/child1_1.jpg" alt="Content"></div>
-</div>
-
-<div class="feed-content-container">
-<div class="feed-account-name"><img src="../users/user1/user1_profile.jpg" alt="Profile picture"> UserAccountName</div>
+<img class="feed-content-auth" src="../users/user1/user1_profile.jpg" alt="Profile picture">
+<div class="feed-account-name">nakkkkkkkme</div>
 <hr>
 <div class="feed-content-media"><img src="../users/user1/child1/child1_1.jpg" alt="Content"></div>
 </div>
 
 <div class="feed-content-container">
-<div class="feed-account-name"><img src="../stock2.jpg" alt="Profile picture"> UserAccountName</div>
+<img class="feed-content-auth" src="../users/user1/user1_profile.jpg" alt="Profile picture">
+<div class="feed-account-name">Jhokn Doe</div>
+<hr>
+<div class="feed-content-media"><img src="../users/user1/child1/child1_1.jpg" alt="Content"></div>
+</div>
+
+<div class="feed-content-container">
+<img class="feed-content-auth" src="../users/user1/user1_profile.jpg" alt="Profile picture">
+<div class="feed-account-name">Jhon Doe</div>
 <hr>
 <div class="feed-content-media"><img src="../stock2.jpg" alt="Content"></div>
 </div>
 
 <div class="feed-content-container">
-<div class="feed-account-name"><img src="../users/user1/user1_profile.jpg" alt="Profile picture"> UserAccountName</div>
+<img class="feed-content-auth" src="../users/user1/user1_profile.jpg" alt="Profile picture">
+<div class="feed-account-name">Jhon Doe</div>
+<hr>
 <div class="feed-content-media"><img src="../users/user1/child1/child1_1.jpg" alt="Content"></div>
 </div>
 
--- public/profile.php
+++ public/profile.php
@@ -2,7 +2,21 @@
 require_once '../includes/header.php';
 ?>
 
-<div class="profile-child-panel"></div>
+<div class="profile-child-panel">
+    <div class="profile-child-container">
+        <img src="../users/user1/user1_profile.jpg">
+        <span>Child Name</span>
+    </div>
+    <div class="profile-child-container">
+        <img src="../users/user1/user1_profile.jpg">
+        <span>Child Name</span>
+    </div>
+    <div class="profile-child-container">
+        <img src="../users/user1/user1_profile.jpg">
+        <span>Child Name</span>
+        <a><img class="profile-plus" src="../plus.png"></a>
+    </div>
+</div>
 
 <?php
 require_once '../includes/footer.php';

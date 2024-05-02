c88b8cd7016bae043919ec5fc11d9bd9616d9d80
GeorgescuRazvan <126411884+GeorgescuRazvan@users.noreply.github.com>
2024-05-02 23:10:01 +0300

    login/register javascript 90% complete

--- public_view/login.php
+++ public_view/login.php
@@ -1,37 +1,38 @@
-<body>
-    <div id="login-frame">
-        <div id="background-animation"></div>
-        <div class="grid">
-            <p id="welcome-text">Welcome!</p>
-            <div id="loginbox">
-                <form action="./loginhandler.php" method="post" id="login-form">
-                    <ul>
-                        <li id="user-name">
-                            <label for="email">Email adress:</label><br />
-                            <input requred type="text" pattern="^[a-z0-9A-Z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" class="user-name-input" name="email" placeholder="Email address" />
-                        </li>
-                        <li id="user-password">
-                            <label for="password">Password:</label><br />
-                            <input requred type="text" pattern="^[a-zA-Z0-9!@#$%^&*()\-_=+|{}[\]:;'<>,.?/~]{8,50}$" class="user-password-input" name="user_password" placeholder="Password" />
-                        </li>
-                        <!-- TEMP: onclick="return false;" -->
-                        <button type="submit" id="login-button" >Log in</button>
-                        <div id="register">
-                            Not registered yet?
-                            <a href="register.php">Register</a>
-                        </div>
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-
 <?php
 require_once '../includes/header.php';
 ?>
 
+<div id="login-frame">
+    <div id="background-animation"></div>
+    <div class="grid">
+        <p id="welcome-text">Welcome!</p>
+        <div id="loginbox">
+            <form action="./handlers/loginhandler.php" method="post" id="login-form">
+                <ul>
+                    <li id="user-name">
+                        <label for="email">Email adress:</label><br />
+                        <input requred type="text" class="user-name-input" name="email" placeholder="Email address" />
+                        <br>
+                        <span id="login-email-error"></span>
+                    </li>
+                    <li id="user-password">
+                        <label for="password">Password:</label><br />
+                        <input requred type="text" class="user-password-input" name="user_password" placeholder="Password" />
+                        <br>
+                        <span id="login-password-error"></span>
+                    </li>
+                    <button type="submit" id="login-button">Log in</button>
+                    <div id="register">
+                        Not registered yet?
+                        <a href="register.php">Register</a>
+                    </div>
+                </ul>
+            </form>
+        </div>
+    </div>
+</div>
 
+<script src="./js/login.js"> </script>
 <?php
 require_once '../includes/footer.php';
 ?>
--- public_view/register.php
+++ public_view/register.php
@@ -1,53 +1,66 @@
-<body>
-    <div id="reg-frame">
-        <div id="reg-background-animation"></div>
-        <div class="reg-grid">
-            <p id="reg-welcome-text">Welcome!</p>
-            <div id="reg-loginbox">
-                <form action="/test-action" method="post" id="reg-login-form">
-                    <ul>
-                        <div id="reg-full-name">
-                            <li id="reg-real-name">
-                                <label for="register_user_fname">First name:</label><br />
-                                <input type="text" class="reg-user-name-input" name="register_user_fname" placeholder="First name" />
-                            </li>
-                            <li id="reg-real-surname">
-                                <label for="register_user_lname">Last name:</label><br />
-                                <input type="text" class="reg-user-name-input" name="register_user_lname" placeholder="Last name" />
-                            </li>
-                        </div>
-                        <li class="reg-user-input">
-                            <label for="register_user_email">Email adress:</label><br />
-                            <input type="text" class="reg-user-name-input" name="register_user_email" placeholder="Email address" />
-                        </li>
-                        <li class="reg-user-input">
-                            <label for="register_user_dob">Date of birth:</label><br />
-                            <input type="date" class="reg-user-name-dob-input" name="register_user_dob" placeholder="Date of birth" />
-                        </li>
-                        <li class="reg-user-input">
-                            <label for="register_user_password">Password:</label><br />
-                            <input type="text" class="reg-user-password-input" name="register_user_password" placeholder="Password" />
-                        </li>
-                        <li class="reg-password-repeat-input">
-                            <label for="register_user_password_repeat">Repeat your password:</label><br />
-                            <input type="text" class="reg-user-password-repeat" name="register_user_password_repeat" placeholder="Repeat password" />
-                        </li>
-                        <!-- TEMP: onclick="return false;" -->
-                        <button type="#" id="reg-register-button" onclick="return false;">Sign up</button>
-                        <!-- <div id="reg-login">
-                            Already have an account?
-                            <a href="#">Login</a>
-                        </div> -->
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-
 <?php
 require_once '../includes/header.php';
 ?>
+
+<div id="reg-frame">
+    <div id="reg-background-animation"></div>
+    <div class="reg-grid">
+        <p id="reg-welcome-text">Welcome!</p>
+        <div id="reg-loginbox">
+            <form action="./handlers/registerhandler.php" method="post" id="reg-login-form">
+                <ul>
+                    <div id="reg-full-name">
+                        <li id="reg-real-name">
+                            <label for="register_user_fname">First name:</label><br />
+                            <input type="text" class="reg-user-name-input" id="reg-fname" name="register_user_fname" placeholder="First name" />
+                            <br>
+                            <span id="login-fname-error"></span>
+                        </li>
+                        <li id="reg-real-surname">
+                            <label for="register_user_lname">Last name:</label><br />
+                            <input type="text" class="reg-user-name-input" id="reg-lname" name="register_user_lname" placeholder="Last name" />
+                            <br>
+                            <span id="login-lname-error"></span>
+                        </li>
+                    </div>
+                    <li class="reg-user-input">
+                        <label for="register_user_email">Email adress:</label><br />
+                        <input type="text" class="reg-user-name-input" id="reg-email" name="register_user_email" placeholder="Email address" />
+                        <br>
+                        <span id="login-email-error"></span>
+                    </li>
+                    <li class="reg-user-input">
+                        <label for="register_user_dob">Date of birth:</label><br />
+                        <input type="date" class="reg-user-name-dob-input" id="reg-dob" name="register_user_dob" placeholder="Date of birth" />
+                        <br>
+                        <span id="login-dob-error"></span>
+                    </li>
+                    <li class="reg-user-input">
+                        <label for="register_user_password">Password:</label><br />
+                        <input type="text" class="reg-user-password-input" id="reg-pass" name="register_user_password" placeholder="Password" />
+                        <br>
+                        <span id="login-password-error"></span>
+                    </li>
+                    <li class="reg-password-repeat-input">
+                        <label for="register_user_password_repeat">Repeat your password:</label><br />
+                        <input type="text" class="reg-user-password-repeat" id="reg-pass-repeat" name="register_user_password_repeat" placeholder="Repeat password" />
+                        <br>
+                        <span id="login-password-repeat-error"></span>
+                    </li>
+                    <br>
+                    <button type="submit" id="login-button">Sign up</button>
+                    <br><br>
+                    <div id="reg-login">
+                        Already have an account?
+                        <a href="./login.php">Login</a>
+                    </div>
+                </ul>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="./js/register.js"></script>
 
 <?php
 require_once '../includes/footer.php';
